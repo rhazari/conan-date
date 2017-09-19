a79aae71009fc87169ac2ad398c9e1dc5b90fb7e
raihan hazarika <raihan.hazarika@keysight.com>
2017-09-19 17:08:50 -0400

    s

--- conanfile.py
+++ conanfile.py
@@ -4,36 +4,39 @@ from conans.tools import download, check_sha256, unzip
 
 class HowardHinnantDate(ConanFile):
     name = 'date'
-    version = '2.1.0'
+    version = '2.2.0'
     description = 'A date and time library based on the C++11/14/17 <chrono> header'
     url = 'https://github.com/rhazari/conan-date'
-    license = 'https://github.com/HowardHinnant/date/blob/master/LICENSE.txt'
+    license = 'https://github.com/rhazari/date/blob/{!s}-cmake/LICENSE.txt'.format(version)
     settings = 'os', 'compiler', 'arch', 'build_type'
+    exports_sources = 'CMakeLists.txt'
+    generators = 'cmake'
 
     @property
     def _archive_dirname(self):
         return 'date-{!s}-cmake'.format(self.version)
 
     def _get_build_dir(self):
-        return os.path.join(os.getcwd(), 'date')
+        return os.getcwd()
 
     def _run_cmake(self):
         extra_defs = {}
         cmake = CMake(self, parallel=True)
-        cmake.configure(defs=extra_defs, build_dir=self._get_build_dir(), source_dir=self._get_build_dir())
+        cmake.configure(defs=extra_defs, build_dir=self._get_build_dir(), source_dir=self.conanfile_directory)
         return cmake
 
     def source(self):
-        download_url = 'https://github.com/rhazari/date/archive/2.1.0-cmake.zip'
+        download_url = 'https://github.com/rhazari/date/archive/{!s}-cmake.zip'.format(self.version)
         download(download_url, 'date.zip')
-        check_sha256('date.zip','aeffe980f1d512305724f063ceb32683913f9319a1eed27788f1d6d8fd32a6ca')
+        check_sha256('date.zip','a5dffa60d2743808c39b511a392e390901fc3eee6d3592c623e6b471695c043c')
         unzip('date.zip')
         os.unlink('date.zip')
         os.rename(self._archive_dirname, 'date')
 
     def build(self):
         cmake = self._run_cmake()    # rerun cmake
-        cmake.build(build_dir=self._get_build_dir())
+        #cmake.build(build_dir=self._get_build_dir())
+        cmake.build()
 
     def package(self):
         self.copy(pattern='*.h', dst='include/date')
